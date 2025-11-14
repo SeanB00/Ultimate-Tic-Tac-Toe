bea91ca1b132d23dac42d2dfd422a049ba058594
Sean Barabash <sean.barabash@gmail.com>
2025-11-14 16:31:05 +0200

    Random vs random is done

--- logic.py
+++ logic.py
@@ -1,8 +1,10 @@
 import numpy as np
 import random
 
-class tic_tac_toe_one_game():
+class game():
     def __init__(self):
+
+        self.board_representation = np.zeros((9,9), dtype=int)
         self.full_board = np.zeros((3, 3, 3, 3), dtype=int)
         self.sub_boards = np.zeros((3, 3), dtype=int)
         self.player_symbol = -1
@@ -10,6 +12,10 @@ class tic_tac_toe_one_game():
         self.empty_places = self.get_empty_places()
         self.empty_sub_places = self.get_empty_sub_places()
         self.curr_board = None
+
+        self.players = {0: "-", 1: "X", -1: "O"}
+
+
 
     def get_empty_places(self):
         return np.array([[{(i, j) for i in range(3) for j in range(3)} for col in range(3)] for row in range(3)])
@@ -33,6 +39,7 @@ class tic_tac_toe_one_game():
                 return -1
 
         # בדיקת עמודות
+
         for col_idx in range(3):
             col_sum = np.sum(board[:, col_idx])
             if col_sum == 3:
@@ -62,55 +69,156 @@ class tic_tac_toe_one_game():
         return len(empty) == 0 and self.check_win(board) == 0
 
 
+    def sub_board_is_done(self, i, j):
+        #checks if there is a win or draw in the sub-board
+        return len(self.empty_places[i][j]) == 0 or self.sub_boards[i][j] != 0
 
-    def is_full(self, i, j):
-        return len(self.empty_places[i][j]) > 0 and self.sub_boards[i][j] == 0
+    def get_global_position(self, board_i, board_j, cell_row, cell_col):
 
+        global_row = board_i * 3 + cell_row
+        global_col = board_j * 3 + cell_col
+        return (global_row, global_col)
 
     def player_random_move(self):
 
+        #The curr_board tells us which board we need to play in, if None then anywhere
 
         # If curr_board is None, choose a random board that has empty places
-
         available_boards = tuple(self.empty_sub_places)
-        if self.curr_board is None:
 
-            if not available_boards:
-                return None  # No moves available
+        if self.curr_board is None:
             board_i, board_j = random.choice(available_boards)
         else:
             board_i, board_j = self.curr_board
             # Check if current board is still playable
-            if self.is_full(board_i, board_j):
-                # Current board is full or won, choose any available board
-                if not available_boards:
-                    return None  # No moves available
-                board_i, board_j = random.choice(available_boards)
 
         # Choose a random empty position in the selected board
-        empty_positions = list(self.empty_places[board_i][board_j])
-        if not empty_positions:
-            return None
+        empty_positions = tuple(self.empty_places[board_i][board_j])
         row, col = random.choice(empty_positions)
 
         # Make the move
         self.full_board[board_i][board_j][row][col] = self.player_symbol
+        self.board_representation[self.get_global_position(board_i, board_j, row, col)] = self.player_symbol
+
+
         self.empty_places[board_i][board_j].remove((row, col))
+        next_board = (row, col)
 
         # Check if this move wins the sub-board
         win_status = self.check_win(self.full_board[board_i][board_j])
-        if win_status != 0:
+        if win_status != 0 or self.tie(self.full_board[board_i][board_j], self.empty_places[board_i][board_j]):
             self.sub_boards[board_i][board_j] = win_status
-        elif self.tie(board_i, board_j):
-            self.sub_boards[board_i][board_j] = 0  # Mark as tie
             self.empty_sub_places.remove((board_i, board_j))
 
-        # Set next board based on the position played
-        next_board = (row, col)
-        # Check if next board is playable
-        if len(self.empty_places[row][col]) > 0 and self.sub_boards[row][col] == 0:
-            self.curr_board = next_board
+        if self.sub_board_is_done(*next_board):
+            self.curr_board = None
         else:
-            self.curr_board = None  # Player can choose any board
+            self.curr_board = next_board
 
-        return (board_i, board_j, row, col)
+    def agent_random_move(self):
+
+        # The curr_board tells us which board we need to play in, if None then anywhere
+
+        # If curr_board is None, choose a random board that has empty places
+        available_boards = tuple(self.empty_sub_places)
+
+        if self.curr_board is None:
+            board_i, board_j = random.choice(available_boards)
+        else:
+            board_i, board_j = self.curr_board
+            # Check if current board is still playable
+
+        # Choose a random empty position in the selected board
+        empty_positions = tuple(self.empty_places[board_i][board_j])
+        row, col = random.choice(empty_positions)
+
+        # Make the move
+        self.full_board[board_i][board_j][row][col] = self.agent_symbol
+        self.board_representation[self.get_global_position(board_i, board_j, row, col)] = self.agent_symbol
+
+        self.empty_places[board_i][board_j].remove((row, col))
+        next_board = (row, col)
+
+        # Check if this move wins the sub-board
+        win_status = self.check_win(self.full_board[board_i][board_j])
+        if win_status != 0 or self.tie(self.full_board[board_i][board_j], self.empty_places[board_i][board_j]):
+            self.sub_boards[board_i][board_j] = win_status
+            self.empty_sub_places.remove((board_i, board_j))
+
+        if self.sub_board_is_done(*next_board):
+            self.curr_board = None
+        else:
+            self.curr_board = next_board
+
+    def check_true_win(self):
+        return self.check_win(self.sub_boards)
+
+    def check_true_tie(self):
+        return len(self.empty_sub_places) == 0 and self.check_true_win() == 0
+
+    def init_game(self):
+        self.full_board = np.zeros((3, 3, 3, 3), dtype=int)
+        self.sub_boards = np.zeros((3, 3), dtype=int)
+        self.empty_places = self.get_empty_places()
+        self.empty_sub_places = self.get_empty_sub_places()
+        self.curr_board = None
+
+
+    def is_game_running(self):
+        return not self.check_true_tie() and self.check_true_win() == 0
+
+
+    def get_true_board_string(self):
+        s = ""
+        boardNumpy = self.board_representation
+        for row in range(9):
+            printable_row = [self.players[x] for x in boardNumpy[row]]
+            s += "".join(printable_row)
+        return s
+
+    def print_board(self):
+        s = ""
+        boardNumpy = self.board_representation
+        for row in range(9):
+            s += "|"
+            for col in range(9):
+                s += self.players[boardNumpy[row][col]]
+                s += "|"
+                if col % 3 == 2 and col != 8:
+                    s += "  "
+                    s += "|"
+            s += '\n'
+            if row % 3 == 2:
+                s += "\n"
+        print(s)
+        print("".join(["-" for i in range(9)]))
+
+
+
+
+
+    def play_one_game(self):
+        self.init_game()
+        self.print_board()
+        while self.is_game_running():
+            self.agent_random_move()
+            self.print_board()
+            if not self.is_game_running():
+                break
+            self.player_random_move()
+            self.print_board()
+
+        winner = self.check_true_win()
+        print(f"winner is {winner}")
+
+
+
+
+
+
+
+
+g = game()
+g.play_one_game()
+
+
