42693401c7972e37b8f1234cded60e7f6d8874c2
Sean Barabash <sean.barabash@gmail.com>
2025-11-30 01:07:46 +0200

    Added pkl

---
+++ pklToLmdb.py
@@ -0,0 +1,18 @@
+# convert_q_pickle_to_lmdb.py
+import pickle, lmdb, struct
+
+KEY_BYTES = 16
+
+def k(x): return x.to_bytes(KEY_BYTES, "big", signed=False)
+def v(val, cnt): return struct.pack("dI", val, cnt)
+
+env = lmdb.open("qtable.lmdb", map_size=1<<40)
+
+with open("q.pkl", "rb") as f:
+    q = pickle.load(f)
+
+with env.begin(write=True) as txn:
+    for s, (val, cnt) in q.items():
+        txn.put(k(s), v(val, cnt))
+
+print("Done.")
