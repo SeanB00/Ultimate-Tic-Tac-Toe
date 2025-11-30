a205aefa36eef8638e786a4b640c21f50b8797ae
Sean Barabash <sean.barabash@gmail.com>
2025-11-30 10:52:34 +0200

    Fixed pkl

--- pklToLmdb.py
+++ pklToLmdb.py
@@ -6,7 +6,15 @@ KEY_BYTES = 32
 def k(x): return x.to_bytes(KEY_BYTES, "big", signed=False)
 def v(val, cnt): return struct.pack("dI", val, cnt)
 
-env = lmdb.open("qtable.lmdb", map_size=1<<40)
+env = lmdb.open(
+    "qtable.lmdb",
+    map_size=1 << 40,
+    subdir=False,       # IMPORTANT: store DB in a single file
+    lock=True,
+    writemap=False,
+    max_readers=4096,
+)
+
 
 with open("q.pkl", "rb") as f:
     q = pickle.load(f)
