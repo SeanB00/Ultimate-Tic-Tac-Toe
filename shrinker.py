a92051799401f964ced8af7f4e698c98cd65a783
Sean Barabash <sean.barabash@gmail.com>
2025-12-07 11:10:13 +0200

    Should work

--- shrinker.py
+++ shrinker.py
@@ -1,6 +1,6 @@
 import lmdb
 
-def shrink(src="qtable.lmdb", dst="qtable_shrink.lmdb"):
+def shrink(dst="qtable.lmdb", src="qtable_shrink.lmdb"):
     env = lmdb.open(src, readonly=True, lock=False, subdir=False)
     env.copy(dst, compact=True)   # compact=True = shrink pages
     env.close()
